52d29fbb9febec980f6065c6ea881a5ff97225e9
Matthias Derer <webmaster@tonstube.de>
2011-09-05 22:39:46 +0200

    remove template comment

--- src/LaidbackObject.php
+++ src/LaidbackObject.php
@@ -1,11 +1,4 @@
 <?php
-/**
- * Created by JetBrains PhpStorm.
- * User: mde
- * Date: 9/5/11
- * Time: 10:29 PM
- * To change this template use File | Settings | File Templates.
- */
  
 class LaidbackObject {
 
